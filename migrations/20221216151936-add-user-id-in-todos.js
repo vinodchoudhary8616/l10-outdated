3f5fc051905a11b07d748c8c6e5c299095dd8f1b
vinodchoudhary8616 <76099534+vinodchoudhary8616@users.noreply.github.com>
2023-01-14 23:29:48 +0530

    Add files via upload

---
+++ migrations/20221216151936-add-user-id-in-todos.js
@@ -0,0 +1,38 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    await queryInterface.addColumn('Todos','userId',{
+      type:Sequelize.DataTypes.INTEGER
+    
+    })
+
+    await queryInterface.addConstraint('Todos',{
+      fields:['userId'],
+      type:'foreign key',
+      references:{
+        table:'Users',
+        field:'id'
+      }
+    })
+    
+    /**
+     * Add altering commands here.
+     *
+     * Example:
+     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
+     */
+  },
+
+  // eslint-disable-next-line no-unused-vars
+  async down (queryInterface, _Sequelize) {
+    await queryInterface.removeColumn('Todos','userId')
+    /**
+     * Add reverting commands here.
+     *
+     * Example:
+     * await queryInterface.dropTable('users');
+     */
+  }
+  };
